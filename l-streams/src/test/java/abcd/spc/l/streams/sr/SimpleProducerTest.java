96268b8fc1596c303973d8f2935725e29cf78dae
akoval <c@apple456>
2020-10-29 16:49:05 +0300

    ss.

---
+++ l-streams/src/test/java/abcd/spc/l/streams/sr/SimpleProducerTest.java
@@ -0,0 +1,12 @@
+package abcd.spc.l.streams.sr;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SimpleProducerTest {
+    @Test
+    void test() {
+        assertEquals(4, 4);
+    }
+}
