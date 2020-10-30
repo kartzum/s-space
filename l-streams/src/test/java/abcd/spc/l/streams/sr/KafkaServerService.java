ec51ec7120e053538055cdd34d4d2a0186853481
akoval <c@apple456>
2020-10-30 16:18:16 +0300

    sr.

---
+++ l-streams/src/test/java/abcd/spc/l/streams/sr/KafkaServerService.java
@@ -0,0 +1,86 @@
+package abcd.spc.l.streams.sr;
+
+import kafka.admin.AdminUtils;
+import kafka.admin.RackAwareMode;
+import kafka.server.KafkaConfig;
+import kafka.server.KafkaServer;
+import kafka.utils.MockTime;
+import kafka.utils.TestUtils;
+import kafka.utils.ZKStringSerializer$;
+import kafka.utils.ZkUtils;
+import kafka.zk.EmbeddedZookeeper;
+import org.I0Itec.zkclient.ZkClient;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.clients.producer.KafkaProducer;
+
+import java.io.Closeable;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.Properties;
+
+public class KafkaServerService implements Closeable {
+    String zkHost = "127.0.0.1";
+    String brokerHost = "127.0.0.1";
+    int brokerPort = 19092;
+
+    KafkaServer kafkaServer;
+    ZkClient zkClient;
+    EmbeddedZookeeper zkServer;
+    ZkUtils zkUtils;
+
+    public void start() {
+        zkServer = new EmbeddedZookeeper();
+        String zkConnect = zkHost + ":" + zkServer.port();
+        zkClient = new ZkClient(
+                zkConnect, 30000, 30000, ZKStringSerializer$.MODULE$);
+        zkUtils = ZkUtils.apply(zkClient, false);
+
+        Properties brokerProps = new Properties();
+        brokerProps.setProperty("zookeeper.connect", zkConnect);
+        brokerProps.setProperty("broker.id", "0");
+        try {
+            brokerProps.setProperty("log.dirs",
+                    Files.createTempDirectory("kafka-").toAbsolutePath().toString());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        brokerProps.setProperty("listeners", "PLAINTEXT://" + brokerHost + ":" + brokerPort);
+        brokerProps.setProperty("offsets.topic.replication.factor", "1");
+        KafkaConfig config = new KafkaConfig(brokerProps);
+        MockTime mock = new MockTime();
+        kafkaServer = TestUtils.createServer(config, mock);
+    }
+
+    @Override
+    public void close() {
+        kafkaServer.shutdown();
+        zkClient.close();
+        zkServer.shutdown();
+    }
+
+    public void createTopic(String topic) {
+        AdminUtils.createTopic(
+                zkUtils, topic, 1, 1, new Properties(), RackAwareMode.Disabled$.MODULE$);
+    }
+
+    public KafkaProducer<Integer, byte[]> createKafkaProducerIntegerByteArray() {
+        Properties producerProps = new Properties();
+        producerProps.setProperty("bootstrap.servers", brokerHost + ":" + brokerPort);
+        producerProps.setProperty("key.serializer", "org.apache.kafka.common.serialization.IntegerSerializer");
+        producerProps.setProperty("value.serializer", "org.apache.kafka.common.serialization.ByteArraySerializer");
+        return new KafkaProducer<Integer, byte[]>(producerProps);
+    }
+
+    public KafkaConsumer<Integer, byte[]> createKafkaConsumerIntegerByteArray() {
+        Properties consumerProps = new Properties();
+        consumerProps.setProperty("bootstrap.servers", brokerHost + ":" + brokerPort);
+        consumerProps.setProperty("group.id", "group0");
+        consumerProps.setProperty("client.id", "consumer0");
+        consumerProps.setProperty(
+                "key.deserializer", "org.apache.kafka.common.serialization.IntegerDeserializer");
+        consumerProps.setProperty(
+                "value.deserializer", "org.apache.kafka.common.serialization.ByteArrayDeserializer");
+        consumerProps.put("auto.offset.reset", "earliest");
+        return new KafkaConsumer<>(consumerProps);
+    }
+}
---
+++ l-streams/src/test/java/abcd/spc/l/streams/sr/SimpleConsumerTest.java
@@ -0,0 +1,34 @@
+package abcd.spc.l.streams.sr;
+
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.junit.jupiter.api.Test;
+
+import java.nio.charset.StandardCharsets;
+import java.time.Duration;
+import java.util.Collections;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SimpleConsumerTest {
+    @Test
+    void test() {
+        try (KafkaServerService service = new KafkaServerService()) {
+            String topic = "topic";
+            String message = "message";
+            service.start();
+            service.createTopic(topic);
+            KafkaProducer<Integer, byte[]> producer = service.createKafkaProducerIntegerByteArray();
+            KafkaConsumer<Integer, byte[]> consumer = service.createKafkaConsumerIntegerByteArray();
+            consumer.subscribe(Collections.singleton(topic));
+            ProducerRecord<Integer, byte[]> data =
+                    new ProducerRecord<>(topic, 42, message.getBytes(StandardCharsets.UTF_8));
+            producer.send(data);
+            producer.close();
+            ConsumerRecords<Integer, byte[]> records = consumer.poll(Duration.ofSeconds(5));
+            assertEquals(1, records.count());
+        }
+    }
+}
